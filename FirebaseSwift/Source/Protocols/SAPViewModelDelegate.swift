ba080720861423fddfca4e633223f215a9ec6af2
Andrew <shevtsov.ap@gmail.com>
2017-01-11 11:19:01 +0200

    Protocols/SAPViewModelDelegate.swift

---
+++ FirebaseSwift/Source/Protocols/SAPViewModelDelegate.swift
@@ -0,0 +1,11 @@
+//
+//  SAPViewModelDelegate.swift
+//  FirebaseSwift
+//
+//  Created by ASH on 1/11/17.
+//  Copyright © 2017 SAP. All rights reserved.
+//
+
+protocol SAPViewModelDelegate: class {
+    func viewModelDidChange(_ viewModel:Any)
+}
