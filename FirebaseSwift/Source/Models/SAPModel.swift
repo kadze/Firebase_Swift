a590d2cae30d92821d9525e99a6baf8355c866a6
Andrew <shevtsov.ap@gmail.com>
2017-01-13 10:51:54 +0200

    SAPModel.swift  ready

--- FirebaseSwift/Source/Models/SAPModel.swift
+++ FirebaseSwift/Source/Models/SAPModel.swift
@@ -6,6 +6,28 @@
 //  Copyright © 2017 SAP. All rights reserved.
 //
 
+import FirebaseDatabase
+
 class SAPModel {
+    //MARK: Properties
+    var reference: FIRDatabaseReference?
     
+    //MARK: Class Methods
+    class func model(snapshot: FIRDataSnapshot) -> Self {
+        return self.init(snapshot: snapshot)
+    }
+    
+    //MARK: Init and Deinit
+    required init(snapshot: FIRDataSnapshot) {
+        
+    }
+    
+    //MARK: Public
+    func dictionary() -> Dictionary<AnyHashable, Any>? {
+        return nil //should be overriden in subclass
+    }
+    
+    func addToDatabase() {
+        //should be overriden in subclass
+    }
 }
